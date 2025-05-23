cf56fb72e522064e64c48d3cb09fff15d132a9d7
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-23 20:10:06 +0800

    feat: add ASCIITitle class and move print_title function into it

--- quiz_game/qg_game_title.py
+++ quiz_game/qg_game_title.py
@@ -1,10 +1,11 @@
 from colorama import init, Fore, Style
 from pyfiglet import Figlet
 
-def print_title():
-    fig = Figlet(font='bulbhead')
-    quiz = fig.renderText('Quiz').splitlines()
-    game = fig.renderText('Game').splitlines()
-    for quiz_line, game_line in zip(quiz, game):
-        print(Fore.MAGENTA + quiz_line + "  " + Fore.CYAN + game_line)
-    print(Style.RESET_ALL)
+class ASCIITitle:
+    def print_title(self):
+        fig = Figlet(font='bulbhead')
+        quiz = fig.renderText('Quiz').splitlines()
+        game = fig.renderText('Game').splitlines()
+        for quiz_line, game_line in zip(quiz, game):
+            print(Fore.MAGENTA + quiz_line + "  " + Fore.CYAN + game_line)
+        print(Style.RESET_ALL)
