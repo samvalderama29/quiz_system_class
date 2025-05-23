2699460c89305dfa42dd87f4826cf984e071f14a
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-23 22:13:04 +0800

    feat: import ASCIITitle, HighScoresManager, and QuizGameManager in qg_menu.py; added self method on the def main_menu and on the if-else user choices

--- quiz_game/qg_menu.py
+++ quiz_game/qg_menu.py
@@ -1,27 +1,35 @@
 import sys
 from colorama import init, Fore, Style
+init(autoreset = True)
+from qg_game_title import ASCIITitle
+from qg_high_scores import HighScoresManager
+from qg_game_manager import QuizGameManager
 
-def main_menu():
-    while True:
-        print_title()
+class QuizMainMenu:
+    def __init__(self, quiz_game_path, high_score_path):
+        self.quiz_game_path = quiz_game_path
+        self.high_score_path = high_score_path
 
-        print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT +  "💡 Welcome to the Quiz Game!")
-        print(Fore.LIGHTWHITE_EX + "📚 Test your general knowledge and see how high you can score!")
+    def main_menu(self):
+        while True:
+            ASCIITitle.print_title()
 
+            print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT +  "💡 Welcome to the Quiz Game!")
+            print(Fore.LIGHTWHITE_EX + "📚 Test your general knowledge and see how high you can score!")
 
-        print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
-        print("1️⃣ Play")
-        print("2️⃣ Check High Scores")
-        print("3️⃣ Exit")
+            print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
+            print("1️⃣ Play")
+            print("2️⃣ Check High Scores")
+            print("3️⃣ Exit")
 
-        user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your option (1/2/3): ")
+            user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your option (1/2/3): ")
 
-        if user_choice == "1":
-            quiz_game_start()
-        elif user_choice == "2":
-            high_score_view()
-        elif user_choice == "3":
-            print(Fore.CYAN + Style.BRIGHT + "👋 Goodbye. Thank you for playing!")
-            sys.exit()
-        else:
-            print(Fore.RED +"❌ Invalid input! Please choose between 1, 2, and 3 only.\n")
+            if user_choice == "1":
+                self.quiz_game_path.quiz_game_start()
+            elif user_choice == "2":
+                self.high_score_path.high_score_view()
+            elif user_choice == "3":
+                print(Fore.CYAN + Style.BRIGHT + "👋 Goodbye. Thank you for playing!")
+                sys.exit()
+            else:
+                print(Fore.RED +"❌ Invalid input! Please choose between 1, 2, and 3 only.\n")
