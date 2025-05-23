a6a1a58e6390b7c8ba136aae79d1deb8482e41a1
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-23 17:46:22 +0800

    feat: created ExitPromptMenu class to handle user exit choices (return to menu or terminate program) in the qc_exit_prompt.py; imported QuizMenuManager from qc_menu.py to enable function calls across modules; initialized ExitPromptMenu in qc_exit_prompt.py and updated line 13 to use self.file_manager; fixed indentation for better readability and code consistency

--- quiz_creator/qc_exit_prompt.py
+++ quiz_creator/qc_exit_prompt.py
@@ -1,14 +1,19 @@
 import sys
 from colorama import init, Fore, Style
 init(autoreset = True)
+from qc_menu import QuizMenuManager
 
-def menu_exit_choice():
-    menu_choice = input(Fore.LIGHTWHITE_EX + "\nWould you like to go back to the menu? (yes/no): ")
-    if menu_choice.lower() == "yes":
-        return
-    elif menu_choice.lower() == "no":
-        print(Fore.CYAN + Style.BRIGHT + "\n👋🏽 Goodbye! Thank you. Quiz Creator closed.")
-        sys.exit()
-    else:
-        print(Fore.RED + "❗ Invalid choice! Returning to main menu.")
-        return
+class ExitPromptMenu:
+    def __init__(self):
+        self.file_manager = QuizMenuManager()
+
+    def menu_exit_choice(self):
+        menu_choice = input(Fore.LIGHTWHITE_EX + "\nWould you like to go back to the menu? (yes/no): ")
+        if menu_choice.lower() == "yes":
+            self.file_manager.main_menu()
+        elif menu_choice.lower() == "no":
+            print(Fore.CYAN + Style.BRIGHT + "\n👋🏽 Goodbye! Thank you. Quiz Creator closed.")
+            sys.exit()
+        else:
+            print(Fore.RED + "❗ Invalid choice! Returning to main menu.")
+            return
