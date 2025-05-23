9cb652242253c2016fd38e62bba80b943506fcb0
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-23 17:22:25 +0800

    feat: imported sys and QuizQuestionManager from qc_question_manager.py to enable cross-file functionality; created a function in qc_menu.py to initialize QuizQuestionManager and provide access to methods for adding, removing, and viewing quiz questions

--- quiz_creator/qc_menu.py
+++ quiz_creator/qc_menu.py
@@ -1,26 +1,33 @@
 from colorama import init, Fore, Style
+init(autoreset = True)
+from qc_question_manager import QuizQuestionManager
+import sys
 
-def main_menu():
-    while True:
-        print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "\n📚 Welcome to the Quiz Creator!")
-        print(Fore.LIGHTWHITE_EX + "💡 This program allows you to create questions and input answer to make a quiz!")
-        print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
+class QuizMenuManager:
+    def __init__(self):
+        self.file_manager = QuizQuestionManager
 
-        print(f"{Fore.GREEN}1. {Fore.RESET}Add a question")
-        print(f"{Fore.CYAN}2. {Fore.RESET}Remove a question")
-        print("3. View all questions")
-        print(f"{Fore.RED}4. {Fore.RESET}Exit")
+    def main_menu():
+        while True:
+            print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "\n📚 Welcome to the Quiz Creator!")
+            print(Fore.LIGHTWHITE_EX + "💡 This program allows you to create questions and input answer to make a quiz!")
+            print(Fore.LIGHTYELLOW_EX + "\nWhat would you like to do?")
 
-        user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your choice (1/2/3/4): ")
+            print(f"{Fore.GREEN}1. {Fore.RESET}Add a question")
+            print(f"{Fore.CYAN}2. {Fore.RESET}Remove a question")
+            print("3. View all questions")
+            print(f"{Fore.RED}4. {Fore.RESET}Exit")
 
-        if user_choice == "1":
-            add_new_question()
-        elif user_choice == "2":
-            remove_question()
-        elif user_choice == "3":
-            view_all_questions()
-        elif user_choice == "4":
-            print(Fore.CYAN + Style.BRIGHT + "\n👋🏽 Goodbye! Thank you. Quiz Creator closed.")
-            break
-        else:
-            print(Fore.RED +"\n❌ Invalid input! Please choose between 1, 2, 3, and 4 only.")
+            user_choice = input(Fore.LIGHTYELLOW_EX + "Enter your choice (1/2/3/4): ")
+
+            if user_choice == "1":
+                add_new_question()
+            elif user_choice == "2":
+                remove_question()
+            elif user_choice == "3":
+                view_all_questions()
+            elif user_choice == "4":
+                print(Fore.CYAN + Style.BRIGHT + "\n👋🏽 Goodbye! Thank you. Quiz Creator closed.")
+                break
+            else:
+                print(Fore.RED +"\n❌ Invalid input! Please choose between 1, 2, 3, and 4 only.")
