7d44d31b1c99454f97302c3b4f83f2bd211cd511
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-23 20:26:11 +0800

    feat: removed unused import: sys; added init(autoreset = True) to initialize the colorama package for automatic terminal style resets; added a function to initialize the file path for high_scores.txt; imported ASCIITitle from qg_game_title.py and updated line 21 into self.game_title.print_title() to display the game title once the program is launched; added self to all high_score-related variables for proper class-based access; fixed indentation for readability and consistency

--- quiz_game/qg_high_scores.py
+++ quiz_game/qg_high_scores.py
@@ -1,28 +1,33 @@
-import sys
 import os
 from colorama import init, Fore, Style
+init(autoreset = True)
+from qg_game_title import ASCIITitle
 
-def save_high_score(player_name, current_score):
-    with open(high_score_file, "a") as file:
-        file.write(f"{player_name}: {current_score}\n")
+class HighScoresManager:
+    def __init__(self, high_score_file):
+        self.high_score_file_path = high_score_file
+        self.game_title = ASCIITitle()
 
+    def save_high_score(self, player_name, current_score):
+        with open(self.high_score_file_path, "a") as file:
+            file.write(f"{player_name}: {current_score}\n")
 
-def high_score_view():
-    if not os.path.exists(high_score_file):
-        print(Fore.YELLOW + "⚠️ No high scores yet.")
-        return
+    def high_score_view(self):
+        if not os.path.exists(self.high_score_file_path):
+            print(Fore.YELLOW + "⚠️ No high scores yet.")
+            return
 
-    print()
-    print_title()
-    print(Fore.LIGHTGREEN_EX + "🏆 High Scores 🏆")
+        print()
+        self.game_title.print_title()
+        print(Fore.LIGHTGREEN_EX + "🏆 High Scores 🏆")
 
-    scores = []
-    with open(high_score_file, "r") as file:
-        for name_line in file:
-            player_name, player_score = name_line.strip().rsplit(":", 1)
-            scores.append((int(player_score), player_name))
+        scores = []
+        with open(self.high_score_file_path, "r") as file:
+            for name_line in file:
+                player_name, player_score = name_line.strip().rsplit(":", 1)
+                scores.append((int(player_score), player_name))
 
-    scores.sort(reverse = True)
+        scores.sort(reverse = True)
 
-    for score, name in scores:
-        print(f"{name}: {score}")
+        for score, name in scores:
+            print(f"{name}: {score}")
