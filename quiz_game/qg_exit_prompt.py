11c363ab8e09f7dc717fb72fe96b3aba9741a9f5
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-05-24 09:02:49 +0800

    feat: remove @staticmethod and add a function in the qg_exit_prompt.py to initialize exit_menu with default None

--- quiz_game/qg_exit_prompt.py
+++ quiz_game/qg_exit_prompt.py
@@ -3,12 +3,16 @@ from colorama import init, Fore, Style
 init(autoreset = True)
 
 class ExitPromptMenu:
-    @staticmethod
-    def menu_exit_choice():
-        from qg_menu import QuizMainMenu
-        menu_choice = input(Fore.LIGHTWHITE_EX + "\nWould you like to go back to the menu? (yes/no): ")
+    def __init__(self, exit_menu = None):
+        self.exit_menu_path = exit_menu
+
+    def menu_exit_choice(self):
+        menu_choice = input(Fore.LIGHTWHITE_EX + "Would you like to go back to the menu? (yes/no): ")
         if menu_choice.lower() == "yes":
-            QuizMainMenu().main_menu()
+            print()
+            if self.exit_menu_path:
+                self.exit_menu_path()
+            return
         elif menu_choice.lower() == "no":
             print(Fore.CYAN + Style.BRIGHT + "\n👋🏽 Goodbye! Thank you. Quiz Creator closed.")
             sys.exit()
